26aa2e5a52c69c3ee5621b5dcb013e87c01e6839
H-Miura-fms <mmyha1289hm@gmail.com>
2024-06-19 12:58:33 +0900

    fix:GithubRepoModel null許容

--- lib/model/github_repo_model.g.dart
+++ lib/model/github_repo_model.g.dart
@@ -11,11 +11,11 @@ _$GithubRepoModelImpl _$$GithubRepoModelImplFromJson(
     _$GithubRepoModelImpl(
       name: json['name'] as String,
       fullName: json['full_name'] as String,
-      description: json['description'] as String,
+      description: json['description'] as String?,
       owner: OwnerModel.fromJson(json['owner'] as Map<String, dynamic>),
       stargazersCount: (json['stargazers_count'] as num).toInt(),
       watchersCount: (json['watchers_count'] as num).toInt(),
-      language: json['language'] as String,
+      language: json['language'] as String?,
       forksCount: (json['forks_count'] as num).toInt(),
       openIssuesCount: (json['open_issues_count'] as num).toInt(),
     );
