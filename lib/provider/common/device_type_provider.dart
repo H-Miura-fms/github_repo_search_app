fa3924437f74aad2f990b200ae5bb9901e8a3f18
H-Miura-fms <mmyha1289hm@gmail.com>
2024-06-24 15:49:59 +0900

    feat:タブレット判定を追加

---
+++ lib/provider/common/device_type_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final deviceTypeProvider =
+    Provider<DeviceType>((_) => throw UnimplementedError());
