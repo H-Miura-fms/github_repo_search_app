8d07a3a33cd12a061af5e5ba1c6411222219a985
H-Miura-fms <mmyha1289hm@gmail.com>
2024-06-24 15:50:28 +0900

    feat:タブレットかどうかで画面構成を変更

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -1,11 +1,38 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:github_repo_search_app/provider/common/device_type_provider.dart';
+import 'package:github_repo_search_app/view/list_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends ConsumerWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final deviceType = ref.read(deviceTypeProvider);
+
+    // タブレットの場合，listと詳細を横並びにする
+    if (deviceType == DeviceType.tablet) {
+      return Scaffold(
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            double listPageWidth = constraints.maxWidth * 0.4;
+
+            return Row(
+              children: [
+                SizedBox(
+                  width: listPageWidth,
+                  child: const ListPage(),
+                ),
+                // 詳細ページ(仮)
+                Expanded(child: Container())
+              ],
+            );
+          },
+        ),
+      );
+    } else {
+      return const ListPage();
+    }
   }
 }
