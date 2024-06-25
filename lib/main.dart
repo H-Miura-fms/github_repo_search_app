94f329222d446d2ed79965764db4cefe0e0057e6
H-Miura-fms <mmyha1289hm@gmail.com>
2024-06-25 20:04:23 +0900

    update:themeの指定，preferenceの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,21 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:github_repo_search_app/core/theme/custom_theme.dart';
 import 'package:github_repo_search_app/provider/common/device_type_provider.dart';
 import 'package:github_repo_search_app/view/main_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+import 'provider/common/shared_preference_provider.dart';
+import 'provider/common/theme_state.dart';
+
+void main() async {
   // 明示的に初期化
   WidgetsFlutterBinding.ensureInitialized();
+
+// sharedPreferencesインスタンス取得
+  SharedPreferences prefs = await SharedPreferences.getInstance();
 
   // デバイスサイズ取得
   Size deviceSize = PlatformDispatcher.instance.views.first.physicalSize /
@@ -28,6 +36,9 @@ void main() {
   runApp(
     ProviderScope(
       overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          prefs,
+        ),
         deviceTypeProvider.overrideWithValue(
             istablet ? DeviceType.tablet : DeviceType.mobile),
       ],
@@ -42,14 +53,14 @@ class MyApp extends ConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, ref) {
+    final isDark = ref.watch(themeStateProvider);
     return ScreenUtilInit(
       minTextAdapt: true,
       child: MaterialApp(
         title: 'github repository search',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: (isDark) ? ThemeMode.dark : ThemeMode.light,
         home: const MainPage(),
       ),
     );
